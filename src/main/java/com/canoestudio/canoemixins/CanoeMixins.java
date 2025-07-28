2a142a03c5b759803097fc2f992429b75367ad45
Lonelyxiya <3438505487@qq.com>
2025-07-28 13:54:05 +0800

    Add screenshot to clipboard feature for client
    Introduces ScreenshotToClipboard, a client-side handler that copies screenshots to the system clipboard when taken. Registers the handler during initialization if running on the client side.

--- src/main/java/com/canoestudio/canoemixins/CanoeMixins.java
+++ src/main/java/com/canoestudio/canoemixins/CanoeMixins.java
@@ -2,8 +2,11 @@ package com.canoestudio.canoemixins;
 
 import com.canoestudio.canoemixins.core.LoadedModChecker;
 import com.canoestudio.canoemixins.modbugfix.railcraft.RCMultiblockPatch;
+import com.canoestudio.canoemixins.util.compat.screenshotclipboard.ScreenshotToClipboard;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -33,6 +36,13 @@ public class CanoeMixins {
     public static @Nullable Logger getLogger(){return logger;}
     public static void logInfo(String log){
         logger.info(log);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        if (event.getSide() == Side.CLIENT) {
+            MinecraftForge.EVENT_BUS.register(ScreenshotToClipboard.class);
+        }
     }
 
 
---
+++ src/main/java/com/canoestudio/canoemixins/util/compat/screenshotclipboard/ScreenshotToClipboard.java
@@ -0,0 +1,50 @@
+package com.canoestudio.canoemixins.util.compat.screenshotclipboard;
+
+
+import net.minecraftforge.client.event.ScreenshotEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+@SideOnly(Side.CLIENT)
+public class ScreenshotToClipboard {
+
+    @SubscribeEvent
+    public static void handleScreenshot(ScreenshotEvent event) {
+        new Thread(() -> {
+            Transferable trans = getTransferableImage(event.getImage());
+            Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
+            c.setContents(trans, null);
+        }).start();
+    }
+
+    private static Transferable getTransferableImage(final BufferedImage bufferedImage) {
+        return new Transferable() {
+            @Override
+            public DataFlavor[] getTransferDataFlavors() {
+                return new DataFlavor[] { DataFlavor.imageFlavor };
+            }
+
+            @Override
+            public boolean isDataFlavorSupported(DataFlavor flavor) {
+                return DataFlavor.imageFlavor.equals(flavor);
+            }
+
+            @Override
+            public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+                if (DataFlavor.imageFlavor.equals(flavor)) {
+                    return bufferedImage;
+                }
+                throw new UnsupportedFlavorException(flavor);
+            }
+        };
+    }
+}
