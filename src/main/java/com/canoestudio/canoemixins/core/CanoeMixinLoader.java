34471fa5e89ab8b499c2cac34b40cabc4618c61a
Lonelyxiya <3438505487@qq.com>
2025-07-31 21:14:46 +0800

    Added durability
    Added durability value display for tools/armor—should, in theory, remain compatible with the majority of mods.

--- src/main/java/com/canoestudio/canoemixins/core/CanoeMixinLoader.java
+++ src/main/java/com/canoestudio/canoemixins/core/CanoeMixinLoader.java
@@ -15,6 +15,7 @@ public class CanoeMixinLoader implements ILateMixinLoader {
         mixins.add("mixin.canoemixins.industrialforegoing.json");
         mixins.add("mixin.canoemixins.dramatictrees.json");
         mixins.add("mixin.canoemixins.forestry.json");
+        mixins.add("mixin.canoemixins.durability.json");
 
         return mixins;
     }
