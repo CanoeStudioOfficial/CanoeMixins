bbedcf201bfb500db728ed794b8c59d3267c3d96
Lonelyxiya <3438505487@qq.com>
2025-08-16 14:25:56 +0800

    remove

--- src/main/java/com/canoestudio/canoemixins/core/CanoeMixinLoader.java
+++ src/main/java/com/canoestudio/canoemixins/core/CanoeMixinLoader.java
@@ -16,7 +16,6 @@ public class CanoeMixinLoader implements ILateMixinLoader {
         mixins.add("mixin.canoemixins.dramatictrees.json");
         mixins.add("mixin.canoemixins.forestry.json");
         mixins.add("mixin.canoemixins.durability.json");
-        mixins.add("mixin.canoemixins.optifinecitpatch.json");
 
         return mixins;
     }
